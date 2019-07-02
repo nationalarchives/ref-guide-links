4bc4b38a2e56d596bb635aac9b097670cd82b106
Gwyn Jones <m@gwnjns.com>
2019-07-02 23:17:09 +0100

    Fixes: variable extraction logging logic

--- app.js
+++ app.js
@@ -4,22 +4,20 @@ const get_variables = require('./get_variables');
 const extract_relevant_section = require('./extract_relevant_section');
 const extract_letter_code_section = require('./extract_letter_code_section');
 
-
-const test_reference = 'ADM 104/140';
-
+const test_reference = 'ADM 137';
 const { letter_code, series, reference } = get_variables(test_reference);
+const extracted_guides = {};
 
-let result = {};
+extracted_guides.letter_code = extract_letter_code_section(letter_code, data);
 
-result.letter_code_section = extract_letter_code_section(letter_code, data);
+if (extracted_guides.letter_code) {
+    extracted_guides.series = extract_relevant_section(series, extracted_guides.letter_code);
 
-if (result.letter_code_section) {
-    result.series_section = extract_relevant_section(series, result.letter_code_section);
-
-    if (result.series_section) {
-        result.reference_section = extract_relevant_section(reference, result.series_section);
+    if (extracted_guides.series) {
+        extracted_guides.reference = extract_relevant_section(reference, extracted_guides.series);
     }
 }
+
 
 
 
