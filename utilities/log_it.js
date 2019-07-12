825449b8e20b3f2dc3b6514492094abee6c757fd
Gwyn Jones <m@gwnjns.com>
2019-07-12 20:22:28 +0100

    Add color to log_it output

--- utilities/log_it.js
+++ utilities/log_it.js
@@ -16,8 +16,8 @@ const print_variable_extraction_table = (test_reference, variables) => {
     console.log(`\nGiven ${test_reference} reference, these ${chalk.blue.bold('variables')} were extracted:`);
 
     const variable_extraction = [
-        ['Extracted letter code', 'Extracted series', 'Full reference'],
-        [variables.letter_code, variables.series, variables.reference]
+        [chalk.yellow.bold('Extracted letter code'), chalk.magenta.bold('Extracted series'), chalk.cyanBright.bold('Full reference')],
+        [chalk.yellow(variables.letter_code), chalk.magenta(variables.series), chalk.cyanBright.bold(variables.reference)]
     ];
 
     const variable_extraction_table = table(variable_extraction, {});
@@ -30,9 +30,9 @@ const print_results =  (results, variables) => {
 
     const variable_extraction = [
         [' ', 'Number of guides', 'Guides'],
-        [variables.letter_code, Object.keys(results.guides_for_letter_code).length, Object.values(results.guides_for_letter_code).join(',\n')],
-        [variables.series, Object.keys(results.guides_for_series).length, Object.values(results.guides_for_series).join(',\n')],
-        [variables.reference, Object.keys(results.guides_for_reference).length, Object.values(results.guides_for_reference).join(',\n')],
+        [chalk.yellow.bold(variables.letter_code), chalk.yellow(Object.keys(results.guides_for_letter_code).length), chalk.yellow(Object.values(results.guides_for_letter_code).join(',\n'))],
+        [chalk.magenta(variables.series), chalk.magenta(Object.keys(results.guides_for_series).length), chalk.magenta(Object.values(results.guides_for_series).join(',\n'))],
+        [chalk.cyanBright(variables.reference), chalk.cyanBright(Object.keys(results.guides_for_reference).length), chalk.cyanBright(Object.values(results.guides_for_reference).join(',\n'))],
 
     ];
 
