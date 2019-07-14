3f2241503a017d7d6cc9557819f599a412a77228
Gwyn Jones <m@gwnjns.com>
2019-07-14 12:47:28 +0100

    Remove icons from log_it utility
    It was causing some layout issues.

--- utilities/log_it.js
+++ utilities/log_it.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 
 module.exports = log_it = (results, test_reference, variables) => {
 
-    process.stdout.write('\033c');
+    process.stdout.write('\033');
 
     print_variable_extraction_table(test_reference, variables);
 
@@ -16,7 +16,7 @@ const print_variable_extraction_table = (test_reference, variables) => {
     console.log(`\nGiven ${chalk.cyanBright.bold(test_reference)} reference, these ${chalk.bold('fragments')} were extracted:`);
 
     const variable_extraction = [
-        [chalk.yellow.bold('🏢 Extracted letter code'), chalk.magenta.bold('📦 Extracted series'), chalk.cyanBright.bold('📜 Full reference')],
+        [chalk.yellow.bold('Extracted letter code'), chalk.magenta.bold('Extracted series'), chalk.cyanBright.bold('Full reference')],
         [chalk.yellow(variables.letter_code), chalk.magenta(variables.series), chalk.cyanBright.bold(variables.reference)]
     ];
 
@@ -30,9 +30,9 @@ const print_results =  (results, variables) => {
 
     const variable_extraction = [
         [' ', 'Number of guides', 'Guides'],
-        [`🏢 ${chalk.yellow.bold(variables.letter_code)}`, chalk.yellow(Object.keys(results.guides_for_letter_code).length), chalk.yellow(Object.values(results.guides_for_letter_code).join(',\n'))],
-        [`📦 ${chalk.magenta(variables.series)}`, chalk.magenta(Object.keys(results.guides_for_series).length), chalk.magenta(Object.values(results.guides_for_series).join(',\n'))],
-        [`📜 ${chalk.cyanBright(variables.reference)}`, chalk.cyanBright(Object.keys(results.guides_for_reference).length), chalk.cyanBright(Object.values(results.guides_for_reference).join(',\n'))],
+        [`${chalk.yellow.bold(variables.letter_code)}`, chalk.yellow(Object.keys(results.guides_for_letter_code).length), chalk.yellow(Object.values(results.guides_for_letter_code).join(',\n'))],
+        [`${chalk.magenta(variables.series)}`, chalk.magenta(Object.keys(results.guides_for_series).length), chalk.magenta(Object.values(results.guides_for_series).join(',\n'))],
+        [`${chalk.cyanBright(variables.reference)}`, chalk.cyanBright(Object.keys(results.guides_for_reference).length), chalk.cyanBright(Object.values(results.guides_for_reference).join(',\n'))],
     ];
 
     const variable_extraction_table = table(variable_extraction, {});
