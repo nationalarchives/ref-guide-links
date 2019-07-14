06fd3d4b9d8dd9cdf1c47d17cba2bd33b1f8db5a
Gwyn Jones <m@gwnjns.com>
2019-07-14 12:49:15 +0100

    Refactor code to extract reference fragments

--- extract_fragments_from_standard_reference.js
+++ extract_fragments_from_standard_reference.js
@@ -1,14 +1,11 @@
-module.exports = extract_fragments_from_standard_reference = (reference) => {
+module.exports = extract_fragments_from_standard_reference = (reference = false) => {
 
-    let letter_code_match = reference.match(/^\w*$/) ? reference.match(/^\w*$/) : reference.match(/\w*?(?=\s)/);
+    let letter_code_match = reference.match(/^\w*(?=\s)/);
     let series_match = reference.match(/\w+\s\d+/);
-    let reference_match = letter_code_match[0] === reference ? false : reference;
 
-    let result = {
+    return result = {
         letter_code: (Array.isArray(letter_code_match)) ? letter_code_match[0] : false,
         series: (Array.isArray(series_match)) ? series_match[0] : false,
-        reference: reference_match
+        reference: reference
     };
-
-    return result;
 };
