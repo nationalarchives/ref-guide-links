02d78d00362f9614d0a035ef94041601201a2ffd
Gwyn Jones <m@gwnjns.com>
2019-07-18 19:28:12 +0100

    Fix bug with lettercode extraction
    MH identified bug where the reference contains only a letter code
    and that letter code is not followed by a space. This commit
    introduces a fix and associated test.

--- extract_fragments_from_standard_reference.js
+++ extract_fragments_from_standard_reference.js
@@ -1,11 +1,11 @@
 module.exports = extract_fragments_from_standard_reference = (reference = false) => {
 
-    let letter_code_match = reference.match(/^\w*(?=\s)/);
-    let series_match = reference.match(/\w+\s\d+/);
+    let letter_code_match = reference.match(/^(\w*)(\s)?/);
+    let series_match = reference.match(/(\w+\s\d+)/);
 
     return result = {
-        letter_code: (Array.isArray(letter_code_match)) ? letter_code_match[0] : false,
-        series: (Array.isArray(series_match)) ? series_match[0] : false,
+        letter_code: (Array.isArray(letter_code_match)) ? letter_code_match[1] : false,
+        series: (Array.isArray(series_match)) ? series_match[1] : false,
         reference: reference
     };
 };
--- test/get_fragments.test.js
+++ test/get_fragments.test.js
@@ -6,7 +6,9 @@ describe('The fragments should be extracted correctly from', function () {
     describe('simple references', function () {
 
         const simple_references = [
-            { reference: "ADM 104/140", expected_letter_code: "ADM", expected_series: "ADM 104" }
+            { reference: "ADM 104/140", expected_letter_code: "ADM", expected_series: "ADM 104" },
+            { reference: "AB ", expected_letter_code: "AB", expected_series: ""},
+            { reference: "AB", expected_letter_code: "AB", expected_series: ""}
         ];
 
         simple_references.forEach(i => {
@@ -14,8 +16,8 @@ describe('The fragments should be extracted correctly from', function () {
 
                 const { letter_code, series, reference } = get_fragments(i.reference);
 
-                assert.equal(letter_code, i.expected_letter_code, `Found a problem with ${i.reference}`);
-                assert.equal(series, i.expected_series, `Found a problem with ${i.reference}`);
+                assert.equal(letter_code, i.expected_letter_code, `Found a problem with ${i.reference} letter_code`);
+                assert.equal(series, i.expected_series, `Found a problem with ${i.reference} series`);
 
             });
 
