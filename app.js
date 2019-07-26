f99b49dafca54dee79d623ea321db1996136a81e
Gwyn Jones <m@gwnjns.com>
2019-07-26 17:32:21 +0100

    Convert to module for import

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const get_guides_for_letter_code = require('./get_guides_for_letter_code');
 const get_guides_for_series = require('./get_guides_for_series');
 const get_guides_for_reference = require('./get_guides_for_reference');
 
-const log_it  = require('./utilities/log_it');
+const log_it = require('./utilities/log_it');
 
-const test_reference = 'PRO 31/8/140B';
-const { letter_code, series, reference } = get_fragments(test_reference);
+module.exports = test_reference => {
+    const { letter_code, series, reference } = get_fragments(test_reference);
 
-let results = {
-    guides_for_letter_code: get_guides_for_letter_code(letter_code, data),
-    guides_for_series: get_guides_for_series(letter_code, series, data),
-    guides_for_reference: get_guides_for_reference(letter_code, series, reference, data)
+    let results = {
+        guides_for_letter_code: get_guides_for_letter_code(letter_code, data),
+        guides_for_series: get_guides_for_series(letter_code, series, data),
+        guides_for_reference: get_guides_for_reference(letter_code, series, reference, data)
+    };
+
+    log_it(results, test_reference, get_fragments(test_reference));
+
+    return results;
 };
 
-log_it(results, test_reference, get_fragments(test_reference));
+
 
 
 
